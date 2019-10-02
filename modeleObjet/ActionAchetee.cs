6881e8177854dc413f06a14744393fae10939acb
Lukas93100 <45822637+Lukas93100@users.noreply.github.com>
2019-10-02 15:32:18 +0200

    création des bonnes classe

--- modeleObjet/ActionAchetee.cs
+++ modeleObjet/ActionAchetee.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ProjetTrader
+namespace modeleObjet
 {
-    public class ActionReelle
+    public class ActionAchetee
     {
         public string CodeAction { get; set; }
         public string NomAction { get; set; }
         public double ValeurAction { get; set; }
+        public int Quantite { get; set; }
+        public double PrixAchat { get; set; }
     }
 }
