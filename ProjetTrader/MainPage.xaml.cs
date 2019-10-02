813ef1381242f12c60245a86555bcc05cac9861c
Lukas93100 <45822637+Lukas93100@users.noreply.github.com>
2019-10-02 16:07:38 +0200

    evolution du xaml

--- ProjetTrader/MainPage.xaml.cs
+++ ProjetTrader/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using modeleObjet;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,14 +23,220 @@ namespace ProjetTrader
     /// </summary>
     public sealed partial class MainPage : Page
     {
+       
         public MainPage()
         {
             this.InitializeComponent();
         }
+        Dictionary<string, List<ActionAchetee>> dico;
+        List<ActionReelle> lesActionsReelles;
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            dico = new Dictionary<string, List<ActionAchetee>>();
+            #region jeu d'essais pour le dico
+            dico.Add
+            ("Enzo", new List<ActionAchetee>()
+            {
+                new ActionAchetee()
+                {
+                    CodeAction = "AAPL",
+                    NomAction = "Apple",
+                    ValeurAction = 200,
+                    PrixAchat = 210,
+                    Quantite = 10
+                },
+                new ActionAchetee()
+                {
+                    CodeAction = "MSFT",
+                    NomAction = "Microsoft",
+                    ValeurAction = 140,
+                    PrixAchat = 100,
+                    Quantite = 50
+                },
+                new ActionAchetee()
+                {
+                    CodeAction = "TWTR",
+                    NomAction = "Twitter",
+                    ValeurAction = 40,
+                    PrixAchat = 35,
+                    Quantite = 20
+                },
+                new ActionAchetee()
+                {
+                    CodeAction = "IBM",
+                    NomAction = "International Business Machines",
+                    ValeurAction = 140,
+                    PrixAchat = 110,
+                    Quantite = 40
+                }
+            }
+            );
+            dico.Add
+            ("Noa", new List<ActionAchetee>()
+                {
+                new ActionAchetee()
+                {
+                    CodeAction = "FB",
+                    NomAction = "Facebook",
+                    ValeurAction = 180,
+                    PrixAchat = 210,
+                    Quantite = 10
+                },
+                new ActionAchetee()
+                {
+                    CodeAction = "AXA",
+                    NomAction = "Axa assurances",
+                    ValeurAction = 25,
+                    PrixAchat = 15,
+                    Quantite = 20
+                },
+                new ActionAchetee()
+                {
+                    CodeAction = "SAP",
+                    NomAction = "SAP SE",
+                    ValeurAction = 120,
+                    PrixAchat = 100,
+                    Quantite = 30
+                }
+            }
+            );
+            dico.Add
+            ("Lilou", new List<ActionAchetee>()
+                {
+                new ActionAchetee()
+                {
+                    CodeAction = "TWTR",
+                    NomAction = "Twitter",
+                    ValeurAction = 40,
+                    PrixAchat = 25,
+                    Quantite = 50
+                },
+                new ActionAchetee()
+                {
+                    CodeAction = "INTC",
+                    NomAction = "Intel Corporation",
+                    ValeurAction = 50,
+                    PrixAchat = 35,
+                    Quantite = 15
+                },
+                new ActionAchetee()
+                {
+                    CodeAction = "VMW",
+                    NomAction = "VMware",
+                    ValeurAction = 145,
+                    PrixAchat = 150,
+                    Quantite = 30
+                },
+                new ActionAchetee()
+                {
+                    CodeAction = "TXN",
+                    NomAction = "Texas Instrument Incorporated",
+                    ValeurAction = 130,
+                    PrixAchat = 140,
+                    Quantite = 25
+                }
+            }
+            );
+            #endregion
+            #region jeu d'essais pour la liste de toutes les actions réelles
+            lesActionsReelles.Add
+            (
+                new ActionReelle()
+                {
+                    CodeAction = "AAPL",
+                    NomAction = "Apple",
+                    ValeurAction = 200
+                }
+            );
+            lesActionsReelles.Add
+            (
+                new ActionReelle()
+                {
+                    CodeAction = "MSFT",
+                    NomAction = "Microsoft",
+                    ValeurAction = 14
+                }
+            );
+            lesActionsReelles.Add
+            (
+                new ActionReelle()
+                {
+                    CodeAction = "TWTR",
+                    NomAction = "Twitter",
+                    ValeurAction = 40
+                }
+            );
+            lesActionsReelles.Add
+            (
+                new ActionReelle()
+                {
+                    CodeAction = "IBM",
+                    NomAction = "International Business Machines",
+                    ValeurAction = 140
+                }
+            );
+            lesActionsReelles.Add
+            (
+                new ActionReelle()
+                {
+                    CodeAction = "FB",
+                    NomAction = "Facebook",
+                    ValeurAction = 180
+                }
+            );
+            lesActionsReelles.Add
+            (
+                new ActionReelle()
+                {
+                    CodeAction = "AXA",
+                    NomAction = "Axa assurances",
+                    ValeurAction = 25
+                }
+            );
+            lesActionsReelles.Add
+            (
+                new ActionReelle()
+                {
+                    CodeAction = "SAP",
+                    NomAction = "SAP SE",
+                    ValeurAction = 120
+                }
+            );
+            lesActionsReelles.Add
+            (
+                new ActionReelle()
+                {
+                    CodeAction = "INTC",
+                    NomAction = "Intel Corporation",
+                    ValeurAction = 50
+                }
+            );
+            lesActionsReelles.Add
+            (
+                new ActionReelle()
+                {
+                    CodeAction = "VMW",
+                    NomAction = "VMware",
+                    ValeurAction = 145
+                }
+            );
+            lesActionsReelles.Add
+            (
+                new ActionReelle()
+                {
+                    CodeAction = "TXN",
+                    NomAction = "Texas Instrument Incorporated",
+                    ValeurAction = 130
+                }
+            );
+            #endregion
 
+        }
+
+        private void LvTrader_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lvTrader.ItemsSource = dico.Keys;
         }
     }
 }
